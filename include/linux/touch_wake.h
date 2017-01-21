a90eb0a006a08b47114c4f8a53c734988a13f47a
VoronFX <Voron.exe@gmail.com>
2017-01-22 00:20:09 +0900

    Another try

--- include/linux/touch_wake.h
+++ include/linux/touch_wake.h
@@ -4,7 +4,6 @@
 #define _LINUX_TOUCH_WAKE_H
 
 #include <linux/input.h>
-#include <linux/gp2a.h>
 
 void powerkey_pressed(void);
 void powerkey_released(void);
@@ -14,6 +13,8 @@ void touch_press(void);
 int get_touchoff_delay(void);
 bool device_is_suspended(void);
 void set_powerkeydev(struct input_dev * input_device);
-static gp2a_platform_data *touch_wake_proximity_dev;
+
+void enable_for_touchwake(void);
+void restore_for_touchwake(void);
 
 #endif
