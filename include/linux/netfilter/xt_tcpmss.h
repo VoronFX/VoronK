3c47c1ebba3ecf5ea1a833f1c0ff9dac4dba7d42
VoronFX <Voron.exe@gmail.com>
2017-01-22 17:21:21 +0900

    Update xt_tcpmss.h

--- include/linux/netfilter/xt_tcpmss.h
+++ include/linux/netfilter/xt_tcpmss.h
@@ -3,6 +3,12 @@
 
 #include <linux/types.h>
 
+struct xt_tcpmss_info {
+	__u16 mss;
+};
+
+#define XT_TCPMSS_CLAMP_PMTU 0xffff
+
 struct xt_tcpmss_match_info {
     __u16 mss_min, mss_max;
     __u8 invert;
