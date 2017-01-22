6bd1631dc77d75d09b2dc70af48ddeb811fa14ad
VoronFX <Voron.exe@gmail.com>
2017-01-22 17:29:34 +0900

    Update ip6t_hl.h

--- include/linux/netfilter_ipv6/ip6t_hl.h
+++ include/linux/netfilter_ipv6/ip6t_hl.h
@@ -20,5 +20,17 @@ struct ip6t_hl_info {
 	__u8	hop_limit;
 };
 
+enum {
+	IP6T_HL_SET = 0,
+	IP6T_HL_INC,
+	IP6T_HL_DEC
+};
+
+#define IP6T_HL_MAXMODE	IP6T_HL_DEC
+
+struct ip6t_HL_info {
+	__u8	mode;
+	__u8	hop_limit;
+};
 
 #endif
